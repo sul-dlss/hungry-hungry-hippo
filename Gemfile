86abd8a4388bd98a43ffdbafbe5bcba449c734b4
Aaron Collier <aaron.collier@stanford.edu>
2024-11-04 21:21:57 -0800

    Add basic test to establish coverage

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri windows], require: 'debug/prelude'
   gem 'erb_lint', require: false
+  gem 'rspec_junit_formatter' # used by CircleCI
   gem 'rspec-rails'
   gem 'rubocop', require: false
   gem 'rubocop-capybara', require: false
---
+++ spec/requests/status_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# This is testing the status page that is provided by the Rails health check gem.
+# This test is being used to establish a coverage baseline for CodeClimat in CI
+# This can be removed once we have actual tests in place.
+RSpec.describe 'Status Page' do
+  describe 'GET /up' do
+    before do
+      get '/up'
+    end
+
+    it 'returns an http OK' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a green background' do
+      expect(response.body).to include('background-color: green')
+    end
+  end
+end
