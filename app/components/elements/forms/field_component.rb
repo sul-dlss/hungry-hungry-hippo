424d2e44d239e46f81c3951565034084f21b1193
Justin Littman <justinlittman@stanford.edu>
2024-11-18 12:39:08 -0500

    Add help text support to text field component.

--- app/components/elements/forms/field_component.rb
+++ app/components/elements/forms/field_component.rb
@@ -4,16 +4,17 @@ module Elements
   module Forms
     # Base component for all form fields.
     class FieldComponent < ApplicationComponent
-      def initialize(form:, field_name:, required: false, hidden_label: false, label: nil)
+      def initialize(form:, field_name:, required: false, hidden_label: false, label: nil, help_text: nil) # rubocop:disable Metrics/ParameterLists
         @form = form
         @field_name = field_name
         @required = required
         @hidden_label = hidden_label
         @label = label
+        @help_text = help_text
         super()
       end
 
-      attr_reader :form, :field_name, :required
+      attr_reader :form, :field_name, :required, :help_text
 
       # Override in subclasses for a different label class
       def label_class
@@ -28,6 +29,16 @@ module Elements
         return field_name if @label.blank?
 
         @label
+      end
+
+      def help_text_id
+        @help_text_id ||= form.field_id(field_name, 'help')
+      end
+
+      def field_aria
+        return if @help_text.blank?
+
+        { describedby: help_text_id }
       end
     end
   end
