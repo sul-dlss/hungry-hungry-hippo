c08b7fa1705e1c7783750cefe7016380dd90291b
Justin Littman <justinlittman@stanford.edu>
2024-11-19 17:25:42 -0500

    Adds authorization.
    closes #24

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@
 
 # Controller for the user dashboard
 class DashboardController < ApplicationController
+  skip_verify_authorized only: :show
+
   def show; end
 end
---
+++ spec/requests/update_work_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Update work' do
+  include MappingFixtures
+
+  let(:druid) { druid_fixture }
+
+  context 'when the user is not authorized' do
+    before do
+      create(:work, druid:)
+      sign_in(create(:user))
+    end
+
+    it 'redirects to root' do
+      put "/works/#{druid}"
+
+      expect(response).to redirect_to(root_path)
+    end
+  end
+end
