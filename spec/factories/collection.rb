1f5c00ce92dc49420070876b0b42edf68e06e1b2
Aaron Collier <aaron.collier@stanford.edu>
2024-11-21 10:27:16 -0800

    Add collection mapper and job specs

--- spec/factories/collection.rb
+++ spec/factories/collection.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     sequence(:title) { |n| "Collection #{n}" }
     druid { 'druid:cc234dd5678' }
     user
+
+    trait :deposit_job_started do
+      deposit_job_started_at { Time.zone.now }
+    end
   end
 end
