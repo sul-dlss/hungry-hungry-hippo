ac1ad8c85d106e40cb71faf242fbcf286517e13c
Justin Littman <justinlittman@stanford.edu>
2024-11-26 16:44:12 -0500

    Test adjustment.

--- spec/services/cocina_support_spec.rb
+++ spec/services/cocina_support_spec.rb
@@ -77,80 +77,22 @@ RSpec.describe CocinaSupport do
   end
 
   describe '#update_version_and_lock' do
-    let(:cocina_object) do
-      dro_with_metadata_fixture.new(structural: {
-                                      contains: [
-                                        {
-                                          type: 'https://cocina.sul.stanford.edu/models/resources/file',
-                                          externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74',
-                                          label: 'My file',
-                                          version: 1,
-                                          structural: {
-                                            contains: [
-                                              {
-                                                type: 'https://cocina.sul.stanford.edu/models/file',
-                                                externalIdentifier: 'https://cocina.sul.stanford.edu/file/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74/my_file1.text',
-                                                label: 'My file1',
-                                                filename: 'my_file1.txt',
-                                                size: 204_615,
-                                                version: 1,
-                                                hasMimeType: 'text/plain',
-                                                sdrGeneratedText: false,
-                                                correctedForAccessibility: false,
-                                                hasMessageDigests: [
-                                                  { type: 'md5', digest: '46b763ec34319caa5c1ed090aca46ef2' },
-                                                  { type: 'sha1', digest: 'd4f94915b4c6a3f652ee7de8aae9bcf2c37d93ea' }
-                                                ],
-                                                access: { view: 'world', download: 'world',
-                                                          controlledDigitalLending: false },
-                                                administrative: { publish: false, sdrPreserve: true, shelve: false }
-                                              }
-                                            ]
-                                          }
-                                        }
-                                      ]
-                                    })
+    subject(:updated_cocina_object) do
+      described_class.update_version_and_lock(cocina_object: cocina_object, version: version, lock: lock)
     end
 
-    let(:version) { 2 }
-    let(:lock) { 'abc123' }
-    let(:expected_cocina_object) do
-      dro_with_metadata_fixture.new(structural: {
-                                      contains: [
-                                        {
-                                          type: 'https://cocina.sul.stanford.edu/models/resources/file',
-                                          externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74',
-                                          label: 'My file',
-                                          version: 2,
-                                          structural: {
-                                            contains: [
-                                              {
-                                                type: 'https://cocina.sul.stanford.edu/models/file',
-                                                externalIdentifier: 'https://cocina.sul.stanford.edu/file/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74/my_file1.text',
-                                                label: 'My file1',
-                                                filename: 'my_file1.txt',
-                                                size: 204_615,
-                                                version: 2,
-                                                hasMimeType: 'text/plain',
-                                                sdrGeneratedText: false,
-                                                correctedForAccessibility: false,
-                                                hasMessageDigests: [
-                                                  { type: 'md5', digest: '46b763ec34319caa5c1ed090aca46ef2' },
-                                                  { type: 'sha1', digest: 'd4f94915b4c6a3f652ee7de8aae9bcf2c37d93ea' }
-                                                ],
-                                                access: { view: 'world', download: 'world',
-                                                          controlledDigitalLending: false },
-                                                administrative: { publish: false, sdrPreserve: true, shelve: false }
-                                              }
-                                            ]
-                                          }
-                                        }
-                                      ]
-                                    })
+    let(:cocina_object) do
+      dro_with_structural_and_metadata_fixture
     end
+
+    let(:version) { 3 }
+    let(:lock) { 'bcd234' }
 
     it 'updates the version and lock' do
-      expect(described_class.update_version_and_lock(cocina_object:, version:, lock:)).to eq(expected_cocina_object)
+      expect(updated_cocina_object.lock).to eq lock
+      expect(updated_cocina_object.version).to eq version
+      expect(updated_cocina_object.structural.contains.first.version).to eq version
+      expect(updated_cocina_object.structural.contains.first.structural.contains.first.version).to eq version
     end
   end
 end
