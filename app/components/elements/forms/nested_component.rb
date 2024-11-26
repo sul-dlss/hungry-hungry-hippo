17bbabf766a16743a08f05a93e26a4362c2e563d
Michael J. Giarlo <mjgiarlo@stanford.edu>
2024-11-26 10:11:17 -0800

    Spike on automagic nested component rendering for simple forms (all strings)

--- app/components/elements/forms/nested_component.rb
+++ app/components/elements/forms/nested_component.rb
@@ -4,7 +4,7 @@ module Elements
   module Forms
     # Encapsulates a nested form, including adding and removing nested models.
     class NestedComponent < ApplicationComponent
-      def initialize(form:, model_class:, field_name:, form_component:)
+      def initialize(form:, model_class:, field_name:, form_component: all_strings_form_component)
         @form = form
         @model_class = model_class
         @field_name = field_name
@@ -20,6 +20,43 @@ module Elements
 
       def header_label
         model_class.model_name.plural.humanize
+      end
+
+      private
+
+      def all_strings_form_component
+        Class.new(ApplicationComponent) do
+          def self.name
+            'NaiveFormComponent'
+          end
+
+          attr_reader :form
+
+          def initialize(form:)
+            @form = form
+
+            if @form.object.class.attribute_types.any? { |_, field_type_instance| field_type_instance.type != :string }
+              raise ArgumentError, "#{@form.object} has one or more non-string field types, so the `form_component:` " \
+                                   'keyword argument must be passed to NestedComponent.'
+            end
+
+            super()
+          end
+
+          def call
+            safe_join(
+              form.object.attribute_names.map do |field_name|
+                render(
+                  Elements::Forms::TextFieldComponent.new(
+                    field_name:,
+                    form:,
+                    label: I18n.t("#{form.object.model_name.plural}.edit.fields.#{field_name}.label")
+                  )
+                )
+              end
+            )
+          end
+        end
       end
     end
   end
