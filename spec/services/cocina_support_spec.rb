4a73ddd80a6f86b9ea61e5b3ebc5c1209952a318
Laura Wrubel <lwrubel@stanford.edu>
2024-11-26 16:32:03 -0500

    Spec

--- spec/services/cocina_support_spec.rb
+++ spec/services/cocina_support_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe CocinaSupport do
+  include WorkMappingFixtures
+
   describe '#title_for' do
     let(:cocina_object) { build(:dro, title: title_fixture) }
 
@@ -71,6 +73,84 @@ RSpec.describe CocinaSupport do
 
     it 'returns the abstract' do
       expect(described_class.abstract_for(cocina_object:)).to eq abstract_fixture
+    end
+  end
+
+  describe '#update_version_and_lock' do
+    let(:cocina_object) do
+      dro_with_metadata_fixture.new(structural: {
+                                      contains: [
+                                        {
+                                          type: 'https://cocina.sul.stanford.edu/models/resources/file',
+                                          externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74',
+                                          label: 'My file',
+                                          version: 1,
+                                          structural: {
+                                            contains: [
+                                              {
+                                                type: 'https://cocina.sul.stanford.edu/models/file',
+                                                externalIdentifier: 'https://cocina.sul.stanford.edu/file/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74/my_file1.text',
+                                                label: 'My file1',
+                                                filename: 'my_file1.txt',
+                                                size: 204_615,
+                                                version: 1,
+                                                hasMimeType: 'text/plain',
+                                                sdrGeneratedText: false,
+                                                correctedForAccessibility: false,
+                                                hasMessageDigests: [
+                                                  { type: 'md5', digest: '46b763ec34319caa5c1ed090aca46ef2' },
+                                                  { type: 'sha1', digest: 'd4f94915b4c6a3f652ee7de8aae9bcf2c37d93ea' }
+                                                ],
+                                                access: { view: 'world', download: 'world',
+                                                          controlledDigitalLending: false },
+                                                administrative: { publish: false, sdrPreserve: true, shelve: false }
+                                              }
+                                            ]
+                                          }
+                                        }
+                                      ]
+                                    })
+    end
+
+    let(:version) { 2 }
+    let(:lock) { 'abc123' }
+    let(:expected_cocina_object) do
+      dro_with_metadata_fixture.new(structural: {
+                                      contains: [
+                                        {
+                                          type: 'https://cocina.sul.stanford.edu/models/resources/file',
+                                          externalIdentifier: 'https://cocina.sul.stanford.edu/fileSet/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74',
+                                          label: 'My file',
+                                          version: 2,
+                                          structural: {
+                                            contains: [
+                                              {
+                                                type: 'https://cocina.sul.stanford.edu/models/file',
+                                                externalIdentifier: 'https://cocina.sul.stanford.edu/file/kb185hz2713-f6bafda8-5719-4f77-bd76-02aaa542de74/my_file1.text',
+                                                label: 'My file1',
+                                                filename: 'my_file1.txt',
+                                                size: 204_615,
+                                                version: 2,
+                                                hasMimeType: 'text/plain',
+                                                sdrGeneratedText: false,
+                                                correctedForAccessibility: false,
+                                                hasMessageDigests: [
+                                                  { type: 'md5', digest: '46b763ec34319caa5c1ed090aca46ef2' },
+                                                  { type: 'sha1', digest: 'd4f94915b4c6a3f652ee7de8aae9bcf2c37d93ea' }
+                                                ],
+                                                access: { view: 'world', download: 'world',
+                                                          controlledDigitalLending: false },
+                                                administrative: { publish: false, sdrPreserve: true, shelve: false }
+                                              }
+                                            ]
+                                          }
+                                        }
+                                      ]
+                                    })
+    end
+
+    it 'updates the version and lock' do
+      expect(described_class.update_version_and_lock(cocina_object:, version:, lock:)).to eq(expected_cocina_object)
     end
   end
 end
