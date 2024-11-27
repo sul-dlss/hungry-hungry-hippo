460f21b782909cbaccb173d3fbea80d574883510
Justin Littman <justinlittman@stanford.edu>
2024-11-27 18:56:11 -0500

    Refactor

---
+++ app/components/elements/tab_form/tab_component.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Elements
+  module TabForm
+    # Component for a tab in a tabbed pane.
+    class TabComponent < ApplicationComponent
+      def initialize(label:, tab_name:, selected: false)
+        @label = label
+        @tab_name = tab_name
+        @selected = selected
+        super()
+      end
+
+      def call
+        tag.div(
+          class: classes,
+          id: "#{tab_name}-tab",
+          data: { bs_toggle: 'tab', bs_target: "##{pane_id}", tab_error_target: 'tab' },
+          type: 'button',
+          'aria-controls': pane_id,
+          'aria-selected': selected?
+        ) do
+          label
+        end
+      end
+
+      attr_reader :label, :tab_name
+
+      def selected?
+        @selected
+      end
+
+      def classes
+        merge_classes('nav-link my-1', selected? ? 'active' : nil)
+      end
+
+      def id
+        "#{tab_name}-tab"
+      end
+
+      def pane_id
+        "#{tab_name}-pane"
+      end
+    end
+  end
+end
---
+++ app/components/elements/tab_form/tab_list_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Elements
+  module TabForm
+    # Component for rendering tabbed navigation for the provided form.
+    class TabListComponent < ApplicationComponent
+      renders_many :tabs, 'Elements::TabForm::TabComponent'
+      renders_many :panes, 'Elements::TabForm::PaneComponent'
+
+      def initialize(model:, hidden_fields: [], classes: [], tab_classes: [])
+        @classes = classes
+        @tab_classes = tab_classes
+        @model = model
+        @hidden_fields = hidden_fields
+        super()
+      end
+
+      attr_reader :model, :hidden_fields
+
+      def classes
+        # Provides d-flex, tabbable-panes as the static default classes
+        # merged with any additional classes passed in.
+        merge_classes(%w[d-flex tabbable-panes], @classes)
+      end
+
+      def tab_classes
+        merge_classes(%w[nav nav-pills flex-column px-2 py-1 me-5 col-3], @tab_classes)
+      end
+    end
+  end
+end
