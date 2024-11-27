f53a5be5a5a94311e74f5e0512883df3e45016a7
Aaron Collier <aaron.collier@stanford.edu>
2024-11-26 17:02:42 -0800

    Add Elements::Layout component tests

---
+++ spec/components/elements/layout/tab_component_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Elements::Layout::TabComponent, type: :component do
+  it 'renders a tab' do
+    render_inline(described_class.new(label: 'Tab 1', tab_name: :tab_one))
+
+    expect(page).to have_css('div#tab_one-tab', text: 'Tab 1')
+  end
+end
