6bfd8e5c7d3812956f94dc922997a37672ada422
Aaron Collier <aaron.collier@stanford.edu>
2024-12-05 09:41:01 -0800

    Add testing for bordered and labelless nested component

--- spec/components/elements/forms/repeatable_nested_component_spec.rb
+++ spec/components/elements/forms/repeatable_nested_component_spec.rb
@@ -5,16 +5,43 @@ require 'rails_helper'
 RSpec.describe Elements::Forms::RepeatableNestedComponent, type: :component do
   subject(:component) do
     described_class.new(form:, field_name: :related_links, model_class: RelatedLinkForm,
-                        form_component: RelatedLinks::EditComponent)
+                        form_component: RelatedLinks::EditComponent, hidden_label:, bordered:)
   end
 
   let(:form) { ActionView::Helpers::FormBuilder.new(nil, work_form, vc_test_controller.view_context, {}) }
   let(:work_form) { WorkForm.new }
 
-  it 'renders the nested component' do
-    render_inline(component)
-    expect(page).to have_text('Related links')
-    expect(page).to have_field('Link text')
-    expect(page).to have_field('URL')
+  context 'when rendering the default component' do
+    let(:hidden_label) { false }
+    let(:bordered) { true }
+
+    it 'renders the nested component' do
+      render_inline(component)
+      expect(page).to have_text('Related links')
+      expect(page).to have_field('Link text')
+      expect(page).to have_field('URL')
+      expect(page).to have_no_css('label.visually-hidden')
+      expect(page).to have_css('div.border-3')
+    end
+  end
+
+  context 'when hiding the label for the component' do
+    let(:hidden_label) { true }
+    let(:bordered) { true }
+
+    it 'does not render the label' do
+      render_inline(component)
+      expect(page).to have_css('label.visually-hidden')
+    end
+  end
+
+  context 'when rendering the component borderless' do
+    let(:hidden_label) { true }
+    let(:bordered) { false }
+
+    it 'does not include the border classes' do
+      render_inline(component)
+      expect(page).to have_no_css('div.border-3')
+    end
   end
 end
