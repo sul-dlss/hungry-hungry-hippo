812b8bac5df3acfcd0c1ca985482944471fb2d81
Aaron Collier <aaron.collier@stanford.edu>
2024-12-05 09:14:37 -0800

    Determine classes for RepeatableNestedComponent by bordered param

--- app/components/elements/forms/repeatable_nested_component.rb
+++ app/components/elements/forms/repeatable_nested_component.rb
@@ -5,13 +5,13 @@ module Elements
     # Encapsulates a repeatable nested form, including adding and removing nested models.
     # NOTE: Use the `NestedComponentPresenter` to invoke this component; do not directly instantiate it.
     class RepeatableNestedComponent < ApplicationComponent
-      def initialize(form:, model_class:, field_name:, form_component:)
+      def initialize(form:, model_class:, field_name:, form_component:, hidden_label: false, bordered: true) # rubocop:disable Metrics/ParameterLists
         @form = form
         @model_class = model_class
         @field_name = field_name
         @form_component = form_component
-        @hidden_label = hide_label
-        @classes = classes
+        @hidden_label = hidden_label
+        @bordered = bordered
         super()
       end
 
@@ -22,7 +22,13 @@ module Elements
       end
 
       def classes
-        merge_classes(@classes)
+        merge_classes(%w[mb-3 p-3 row], bordered_classes)
+      end
+
+      def bordered_classes
+        return [] unless @bordered
+
+        %w[border border-3 border-light-subtle border-opacity-75]
       end
 
       def label_text
