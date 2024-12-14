9cd5851f6ed8c5d0bfd563b593201a91e65a98a6
Aaron Collier <aaron.collier@stanford.edu>
2024-12-13 16:17:12 -0800

    Add initial bibtex service

--- app/services/to_bib_tex/citation.rb
+++ app/services/to_bib_tex/citation.rb
@@ -9,13 +9,25 @@ module ToBibTex
 
     def initialize(cocina_object:)
       @cocina_object = cocina_object
+      @bibliography = BibTeX::Bibliography.new
+      @cp = CiteProc::Processor.new style: 'apa', format: 'text'
     end
 
     def call
       # work_type, work_subtypes = CocinaSupport.work_type_and_subtypes_for(cocina_object:)
+      bibliography << cocina_to_bibtex
+      cp.import bibliography.to_citeproc
+      cp.render :bibliography, id: druid_sym
+    end
+
+    private
+
+    attr_reader :cocina_object, :bibliography, :cp
+
+    def cocina_to_bibtex
       BibTeX::Entry.new do |entry|
         entry.type = :misc
-        entry.key = cocina_object.externalIdentifier
+        entry.key = druid_sym
         entry.title = CocinaSupport.title_for(cocina_object:)
         entry.author = CocinaSupport.authors_for(cocina_object:).map { |author| [author['last_name'], author['first_name']].join(', ') }.join(' and ')
         entry.year = CocinaSupport.event_date_for(cocina_object:, type: 'publication')[:year]
@@ -23,8 +35,8 @@ module ToBibTex
       end
     end
 
-    private
-
-    attr_reader :cocina_object
+    def druid_sym
+      cocina_object.externalIdentifier.split(':').last.to_sym
+    end
   end
 end
--- spec/services/to_bib_tex/citation_spec.rb
+++ spec/services/to_bib_tex/citation_spec.rb
@@ -4,11 +4,11 @@ require 'rails_helper'
 
 RSpec.describe ToBibTex::Citation, type: :mapping do
   subject(:bibtex) { described_class.call(cocina_object:) }
-
+ 
   let(:cocina_object) { dro_with_metadata_fixture }
+  let(:citation) { 'My title. (2024).' }
 
   it 'maps to BibTex' do
-    debugger
-    expect(bibtex).to equal(bibtex)
+    expect(bibtex.first.to_s).to equal(citation)
   end
 end
