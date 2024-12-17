c9c6e418603c9ecaa7b546770836421499bc2723
Justin Littman <justinlittman@stanford.edu>
2024-12-17 09:03:55 -0500

    Refactors cocina support for contributors.
    refs #364

---
+++ app/services/cocina_generators/description/organization_contributor.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module CocinaGenerators
+  class Description
+    # Generates the Cocina contributor parameters for an organization
+    class OrganizationContributor
+      def self.call(...)
+        new(...).call
+      end
+
+      # @param name [String] the name of the organization
+      # @param role [String] the role of the organization from ROLES
+      # @param primary [Boolean] whether this is the first author
+      def initialize(name:, role:, primary: false)
+        @name = name
+        @role = role
+        @primary = primary
+      end
+
+      def call
+        {
+          name: [{ value: name }],
+          type: 'organization',
+          role: role_params,
+          status: ('primary' if primary)
+        }.compact
+      end
+
+      private
+
+      attr_reader :name, :role, :primary
+
+      def role_params
+        cocina_role = ContributorRole.call(role:)
+        return if cocina_role.nil?
+
+        [cocina_role]
+      end
+    end
+  end
+end
