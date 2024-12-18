e09d5f3b4b984d52ca1fbb8e85b4fa0fb7b899f8
Michael J. Giarlo <mjgiarlo@stanford.edu>
2024-12-18 14:12:29 -0800

    Remove bogus aliases in collection policy
    I don't think we want to `alias_rule` *and* `def` the same policies.

--- app/policies/collection_policy.rb
+++ app/policies/collection_policy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CollectionPolicy < ApplicationPolicy
-  alias_rule :new?, :create?, :show?, :update?, :edit?, :wait?, :destroy?, to: :manage?
+  alias_rule :show?, :update?, :edit?, :wait?, :destroy?, to: :manage?
 
   def manage?
     record.user_id == user.id
