28384d8cfd939dd73d71e66d85bac8a6e1521815
Justin Littman <justinlittman@stanford.edu>
2024-12-20 14:02:49 -0500

    Adds additional discard draft buttons.
    closes #331

---
+++ spec/system/edit_discard_work_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Discard a work' do
+  include WorkMappingFixtures
+
+  let(:druid) { druid_fixture }
+  let(:user) { create(:user) }
+  let(:cocina_object) do
+    dro_with_structural_and_metadata_fixture
+  end
+  let(:version_status) do
+    VersionStatus.new(status:
+    instance_double(Dor::Services::Client::ObjectVersion::VersionStatus, open?: true, openable?: false,
+                                                                         discardable?: true,
+                                                                         version: cocina_object.version))
+  end
+
+  before do
+    allow(Sdr::Repository).to receive(:find).with(druid:).and_return(cocina_object)
+    allow(Sdr::Repository).to receive(:status).with(druid:).and_return(version_status)
+    allow(Sdr::Repository).to receive(:discard_draft)
+
+    collection = create(:collection, user:, druid: collection_druid_fixture)
+    create(:work, druid:, user:, collection:)
+
+    sign_in(user)
+  end
+
+  it 'discards a draft' do
+    visit edit_work_path(druid)
+
+    find('.nav-link', text: 'Deposit').click
+    accept_confirm(/Are you sure/) do
+      click_on('Discard draft')
+    end
+
+    expect(page).to have_current_path(work_path(druid))
+    expect(page).to have_css('.alert-success', text: 'Draft discarded.')
+    expect(Sdr::Repository).to have_received(:discard_draft).with(druid:)
+  end
+end
--- spec/system/show_discard_work_draft_spec.rb
+++ spec/system/show_discard_work_draft_spec.rb
@@ -2,13 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Discard a collection draft' do
+RSpec.describe 'Discard a work draft' do
+  include WorkMappingFixtures
   include CollectionMappingFixtures
 
-  let(:druid) { collection_druid_fixture }
+  let(:druid) { druid_fixture }
   let(:user) { create(:user) }
-  let!(:collection) { create(:collection, druid:, title: collection_title_fixture, user:) }
-  let(:cocina_object) { collection_with_metadata_fixture }
+  let(:collection) { create(:collection, :with_druid, user:) }
+  let!(:work) { create(:work, druid:, title: title_fixture, collection:, user:) }
+  let(:cocina_object) { dro_with_metadata_fixture }
 
   before do
     allow(Sdr::Repository).to receive(:find).with(druid:).and_return(cocina_object)
@@ -36,16 +38,16 @@ RSpec.describe 'Discard a collection draft' do
     end
 
     it 'discards a draft' do
-      visit collection_path(druid)
+      visit work_path(druid)
 
-      expect(page).to have_css('h1', text: collection.title)
+      expect(page).to have_css('h1', text: work.title)
       expect(page).to have_css('.status', text: 'New version in draft')
-      expect(page).to have_link('Edit or deposit', href: edit_collection_path(druid))
-      accept_confirm 'Are you sure?' do
+      expect(page).to have_link('Edit or deposit', href: edit_work_path(druid))
+      accept_confirm(/Are you sure/) do
         click_on('Discard draft')
       end
 
-      expect(page).to have_current_path(collection_path(druid))
+      expect(page).to have_current_path(work_path(druid))
       expect(page).to have_no_button('Discard draft')
       expect(page).to have_css('.alert-success', text: 'Draft discarded.')
       expect(page).to have_css('.status', text: 'Deposited')
@@ -59,25 +61,30 @@ RSpec.describe 'Discard a collection draft' do
                                                                            openable?: false))
     end
 
+    let(:collection_cocina_object) { collection_with_metadata_fixture }
+
     before do
       allow(Sdr::Repository).to receive(:status).with(druid:).and_return(version_status)
+      allow(Sdr::Repository).to receive(:status).with(druid: collection.druid).and_return(version_status)
+      allow(Sdr::Repository).to receive(:find).with(druid: collection.druid).and_return(collection_cocina_object)
     end
 
     it 'destroys the work' do
-      visit collection_path(druid)
+      visit work_path(druid)
 
-      expect(page).to have_css('h1', text: collection.title)
+      expect(page).to have_css('h1', text: work.title)
       expect(page).to have_css('.status', text: 'Draft - Not deposited')
-      expect(page).to have_link('Edit or deposit', href: edit_collection_path(druid))
-      accept_confirm 'Are you sure?' do
+      expect(page).to have_link('Edit or deposit', href: edit_work_path(druid))
+      accept_confirm(/Are you sure/) do
         click_on('Discard draft')
       end
 
+      expect(page).to have_current_path(work_path(druid))
       expect(page).to have_no_button('Discard draft')
       expect(page).to have_css('.alert-success', text: 'Draft discarded.')
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_current_path(collection_path(druid: collection.druid))
 
-      expect(Collection.find_by(druid:)).to be_nil
+      expect(Work.find_by(druid:)).to be_nil
     end
   end
 end
