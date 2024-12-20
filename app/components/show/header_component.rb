58bb2837b2a26d24f36dfd8e87bdcc786d104ed9
Justin Littman <justinlittman@stanford.edu>
2024-12-20 09:03:54 -0500

    Refactor header component.

--- app/components/show/header_component.rb
+++ app/components/show/header_component.rb
@@ -7,27 +7,8 @@ module Show
       @presenter = presenter
       super()
     end
-    # def initialize(title:, status_message:, edit_path:, destroy_path:, editable: false, discardable: false)
-    #   @title = title
-    #   @status_message = status_message
-    #   @edit_path = edit_path
-    #   @editable = editable
-    #   @destroy_path = destroy_path
-    #   @discardable = discardable
-    #   super()
-    # end
-
-    # attr_reader :title, :status_message, :edit_path, :destroy_path
     attr_reader :presenter
 
     delegate :title, :status_message, to: :presenter
-
-    # def discardable?
-    #   @discardable
-    # end
-
-    # def editable?
-    #   @editable
-    # end
   end
 end
--- spec/components/show/header_component_spec.rb
+++ spec/components/show/header_component_spec.rb
@@ -3,14 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Show::HeaderComponent, type: :component do
+  let(:presenter) do
+    WorkPresenter.new(work_form:, version_status:, work: nil)
+  end
+  let(:work_form) { WorkForm.new(druid: druid_fixture, title:) }
+  let(:version_status) do
+    instance_double(VersionStatus, editable?: editable, discardable?: discardable, status_message:)
+  end
   let(:title) { 'My Title' }
   let(:status_message) { 'Depositing' }
-  let(:edit_path) { '/edit/me' }
-  let(:destroy_path) { '/destroy/me' }
+  let(:editable) { false }
+  let(:discardable) { false }
 
   it 'renders the header' do
-    render_inline(described_class.new(title:, status_message:, edit_path:, destroy_path:, editable: false,
-                                      discardable: false))
+    render_inline(described_class.new(presenter:))
     expect(page).to have_css('h1', text: title)
     expect(page).to have_css('.status', text: status_message)
     expect(page).to have_no_link('Edit or deposit')
@@ -18,19 +24,21 @@ RSpec.describe Show::HeaderComponent, type: :component do
   end
 
   context 'when editable' do
+    let(:editable) { true }
+
     it 'renders the edit button' do
-      render_inline(described_class.new(title:, status_message:, edit_path:, destroy_path:, editable: true,
-                                        discardable: false))
-      expect(page).to have_link('Edit or deposit', href: edit_path)
+      render_inline(described_class.new(presenter:))
+      expect(page).to have_link('Edit or deposit', href: "/works/#{druid_fixture}/edit")
     end
   end
 
   context 'when discardable' do
+    let(:discardable) { true }
+
     it 'renders the discard button' do
-      render_inline(described_class.new(title:, status_message:, edit_path:, destroy_path:, editable: false,
-                                        discardable: true))
+      render_inline(described_class.new(presenter:))
       expect(page).to have_button('Discard draft')
-      expect(page).to have_css('form[action="/destroy/me"]')
+      expect(page).to have_css("form[action=\"/works/#{druid_fixture}\"]")
       expect(page).to have_field('_method', with: 'delete', type: :hidden)
     end
   end
