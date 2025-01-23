5e1a34b7a8825196dd89ebed5d5ed9c6bc0820ac
Nick Budak <budak@stanford.edu>
2025-01-23 13:40:34 -0800

    Pin bootstrap and popper in the importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,7 +8,8 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin 'stimulus-autocomplete'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'bootstrap', to: 'bootstrap.min.js'
+pin '@popperjs/core', to: 'https://cdn.skypack.dev/@popperjs/core@2.11.8'
+pin 'bootstrap', to: 'https://cdn.skypack.dev/bootstrap@5.3.3'
 # Pins for dropzone
 pin 'dropzone' # @6.0.0
 pin 'just-extend' # @5.1.1
