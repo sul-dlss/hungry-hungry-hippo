483b25a776fc7248e9a37fff44fc9c7aa9c5b78e
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-01-31 16:30:09 -0800

    Move bootstrap imports after tweaking the importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails'
 import 'controllers'
-import * as bootstrap from 'bootstrap' // eslint-disable-line no-unused-vars
