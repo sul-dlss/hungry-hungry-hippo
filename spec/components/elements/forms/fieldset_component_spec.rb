a68459d9a39bbf3e2d10129981d0fb6f031423fb
Justin Littman <justinlittman@stanford.edu>
2025-02-11 08:21:09 -0500

    Extract fieldset component.
    refs #606

---
+++ spec/components/elements/forms/fieldset_component_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Elements::Forms::FieldsetComponent, type: :component do
+  it 'renders the fieldset with a label' do
+    render_inline(described_class.new(
+                    label: 'My Fieldset', classes: 'class1', legend_classes: 'class2', label_classes: 'class3',
+                    data: { my_data: true }
+                  )) { '<p>Fieldset content</p>'.html_safe }
+
+    expect(page).to have_css('fieldset.form-fieldset.class1[data-my-data="true"] legend.class2 label.form-label.fw-bold.class3', # rubocop:disable Layout/LineLength
+                             text: 'My Fieldset')
+    expect(page).to have_css('fieldset p', text: 'Fieldset content')
+  end
+
+  context 'when the label is hidden' do
+    it 'renders the fieldset with a hidden label' do
+      render_inline(described_class.new(label: 'My Fieldset', hidden_label: true))
+
+      expect(page).to have_css('fieldset.form-fieldset label.visually-hidden', text: 'My Fieldset')
+    end
+  end
+end
