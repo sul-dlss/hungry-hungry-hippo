5e1820e7f9250c397c16cee6f4fb50da998be13f
Justin Littman <justinlittman@stanford.edu>
2025-02-11 10:41:06 -0500

    Make title field resizable.
    refs #568

--- app/components/elements/forms/textarea_field_component.rb
+++ app/components/elements/forms/textarea_field_component.rb
@@ -4,6 +4,12 @@ module Elements
   module Forms
     # Component for form textarea fields
     class TextareaFieldComponent < FieldComponent
+      def initialize(rows: nil, **args)
+        @rows = rows
+        super(**args)
+      end
+
+      attr_reader :rows
     end
   end
 end
