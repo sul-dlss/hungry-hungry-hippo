e5dd8ee12aef89ab9795bcf206eb25b44c9e2bca
Justin Littman <justinlittman@stanford.edu>
2025-02-12 16:01:55 -0500

    Disable lazy turbo frames for tests.

--- app/components/structure/terms_of_deposit_modal_component.rb
+++ app/components/structure/terms_of_deposit_modal_component.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module Structure
+  # Component for rendering the terms of deposit modal
   class TermsOfDepositModalComponent < ApplicationComponent
+    def loading
+      # Lazy loading is problematic in tests, so we disable it there.
+      Rails.env.test? ? nil : 'lazy'
+    end
   end
 end
