98e16e24c0556d1755983eb01889d5491df5fe5d
Laura Wrubel <lwrubel@stanford.edu>
2025-02-13 09:31:01 -0500

    Add prompt to release duration selection

--- app/javascript/controllers/release_controller.js
+++ app/javascript/controllers/release_controller.js
@@ -6,21 +6,35 @@ export default class extends Controller {
   connect () {
     if (this.releaseImmediateTarget.checked) {
       this.resetReleaseDuration()
+    } else {
+      this.insertPromptOption()
     }
   }
 
   resetReleaseDuration () {
-    // If the release_option radio button selection is "immediate",
-    // the release_duration field should be reset to empty and a prompt.
+    // Reset the release_duration dropdown to a prompt, or create prompt if it does not exist
     const durationDropdown = this.releaseDurationTarget.querySelector('select')
     if (durationDropdown.querySelector('option[value=""]')) {
       durationDropdown.value = ''
+      durationDropdown.selected = true
     } else { // the dropdown needs to be reset to show "Select an option"
-      const blankDuration = document.createElement('option')
-      blankDuration.value = ''
-      blankDuration.text = 'Select an option'
-      blankDuration.selected = true
-      durationDropdown.insertBefore(blankDuration, durationDropdown.firstChild)
+      this.insertPromptOption()
     }
+  }
+
+  insertPromptOption () {
+    // Prompt option is not included in the dropdown when loading a saved collection
+    // or when fails validation so add it back for consistency, with appropriate selection.
+    const durationDropdown = this.releaseDurationTarget.querySelector('select')
+    const blankDuration = document.createElement('option')
+    blankDuration.value = ''
+    blankDuration.text = 'Select an option'
+    blankDuration.selected = false
+    if (this.releaseImmediateTarget.checked) {
+      blankDuration.selected = true
+    } else if (durationDropdown.classList.contains('is-invalid')) {
+      blankDuration.selected = true
+    }
+    durationDropdown.insertBefore(blankDuration, durationDropdown.firstChild)
   }
 }
