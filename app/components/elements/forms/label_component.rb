7090eacdc33d4d4f6c4c31522d1165ddf78e732f
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-02-13 13:31:32 -0800

    Implement tooltips
    Fixes #450

--- app/components/elements/forms/label_component.rb
+++ app/components/elements/forms/label_component.rb
@@ -5,17 +5,27 @@ module Elements
     # Component for rendering a form label.
     class LabelComponent < ApplicationComponent
       def initialize(form:, field_name:, label_text: nil, default_label_class: 'form-label', hidden_label: false, # rubocop:disable Metrics/ParameterLists
-                     classes: [])
+                     classes: [], tooltip: nil)
         @form = form
         @label_text = label_text
         @field_name = field_name
         @hidden_label = hidden_label
         @default_label_class = default_label_class
         @classes = classes
+        @tooltip = tooltip
         super()
       end
 
       attr_reader :field_name, :form
+
+      def render_tooltip?
+        tooltip.present?
+      end
+
+      def tooltip
+        # The tooltip can include a trailing \n that isn't rendered in the HTML but does make for ugly source markup
+        @tooltip&.chomp
+      end
 
       def label_text
         return field_name if @label_text.blank?
