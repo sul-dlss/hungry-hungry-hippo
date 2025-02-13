82f51e40a5b786f1cd7624dbf95cff1ccb0081fc
Justin Littman <justinlittman@stanford.edu>
2025-02-13 12:49:11 -0500

    Align dashboard heading levels.
    closes #574

--- app/components/collections/header_link_component.rb
+++ app/components/collections/header_link_component.rb
@@ -3,10 +3,18 @@
 module Collections
   # Component for rendering a link to a collection as a header component.
   class HeaderLinkComponent < ApplicationComponent
-    def initialize(collection:, level:)
+    def initialize(collection:)
       @collection = collection
       @level = level
       super()
+    end
+
+    def level
+      :h3
+    end
+
+    def classes
+      'h4'
     end
   end
 end
--- spec/components/collections/header_link_component_spec.rb
+++ spec/components/collections/header_link_component_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe Collections::HeaderLinkComponent, type: :component do
     let(:collection) { create(:collection, user:, druid: nil) }
 
     it 'renders the collections header without a link' do
-      render_inline(described_class.new(collection:, level: :h4))
+      render_inline(described_class.new(collection:))
 
-      expect(page).to have_css('h4', text: collection.title)
+      expect(page).to have_css('h3.h4', text: collection.title)
       expect(page).to have_no_css('a')
     end
   end
@@ -20,9 +20,9 @@ RSpec.describe Collections::HeaderLinkComponent, type: :component do
     let(:collection) { create(:collection, user:, druid: 'druid:ab234dd5678') }
 
     it 'renders the collections header with a link' do
-      render_inline(described_class.new(collection:, level: :h4))
+      render_inline(described_class.new(collection:))
 
-      expect(page).to have_css('h4', text: collection.title)
+      expect(page).to have_css('h3.h4', text: collection.title)
       expect(page).to have_css('a')
     end
   end
