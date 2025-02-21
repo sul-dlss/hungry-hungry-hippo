e447cfde124b895241b17ab6477d7939d21100ee
Aaron Collier <aaron.collier@stanford.edu>
2025-02-21 14:05:25 -0800

    Contact email cannot be blank

--- app/forms/contact_email_form.rb
+++ app/forms/contact_email_form.rb
@@ -6,7 +6,7 @@ class ContactEmailForm < ApplicationForm
   validates :email, presence: true, on: :deposit
   validates :email, format: {
     with: URI::MailTo::EMAIL_REGEXP,
-    allow_blank: true,
+    allow_blank: false,
     message: I18n.t('contact_email.validation.email.invalid')
   }
 end
