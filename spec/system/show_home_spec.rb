c8d926505705d4a14f192570aacad8d4009ce3e7
Aaron Collier <aaron.collier@stanford.edu>
2025-02-25 16:03:00 -0800

    Redirect authorized users to the dashboard from the homepage

--- spec/system/show_home_spec.rb
+++ spec/system/show_home_spec.rb
@@ -4,18 +4,33 @@ require 'rails_helper'
 
 RSpec.describe 'Show home', :rack_test do
   include ActionView::Helpers::SanitizeHelper
-  before do
-    sign_in(create(:user))
+
+  context 'when the user is not authorized to see the dashboard' do
+    before do
+      sign_in(create(:user))
+    end
+
+    it 'shows the home page' do
+      visit root_path
+
+      expect(page).to have_content('Create or manage your deposits')
+      expect(page).to have_link('Enter here', href: dashboard_path)
+      expect(page).to have_content(strip_links(I18n.t('banner.home_html')))
+      expect(page).to have_content('Sign up for our newsletter')
+      expect(page).to have_css('.quote-card', text: 'I am definitely hearing more')
+      expect(page).to have_css('.quote-card', count: 5)
+    end
   end
 
-  it 'shows the home page' do
-    visit root_path
+  context 'when the user is authorized to see the dashboard' do
+    before do
+      sign_in(create(:user), groups: [Settings.authorization_workgroup_names.collection_creators])
+    end
 
-    expect(page).to have_content('Create or manage your deposits')
-    expect(page).to have_link('Enter here', href: dashboard_path)
-    expect(page).to have_content(strip_links(I18n.t('banner.home_html')))
-    expect(page).to have_content('Sign up for our newsletter')
-    expect(page).to have_css('.quote-card', text: 'I am definitely hearing more')
-    expect(page).to have_css('.quote-card', count: 5)
+    it 'redirects to the dashboard' do
+      visit root_path
+
+      expect(page).to have_current_path(dashboard_path)
+    end
   end
 end
