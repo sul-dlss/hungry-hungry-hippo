025f046b23d3b36091ab290e4782e35efdec6033
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-02-25 17:36:21 -0800

    Restrict length of file description and whats changing fields
    Fixes #683

--- app/components/elements/forms/text_field_component.rb
+++ app/components/elements/forms/text_field_component.rb
@@ -4,9 +4,26 @@ module Elements
   module Forms
     # Component for form text fields
     class TextFieldComponent < FieldComponent
-      def initialize(**args)
+      # This is the default `size` attribute of the `<input>` element. See https://www.w3schools.com/TAGs/att_input_size.asp
+      DEFAULT_SIZE = 20
+
+      def initialize(maxlength: nil, size: nil, **args)
+        @maxlength = maxlength
+        @size = size
         args[:container_classes] = merge_classes('field-container', args[:container_classes])
-        super
+        super(**args)
+      end
+
+      attr_reader :maxlength, :rows
+
+      # If `maxlength` is set on this component and a size is not, the size of
+      # the input element will default (in HTML) to the value of the `maxlength`
+      # attribute. In that case, we want to inject the default size back into
+      # the input element.
+      def size
+        return if maxlength.blank?
+
+        DEFAULT_SIZE
       end
     end
   end
