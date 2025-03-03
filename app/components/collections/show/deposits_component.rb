e1800c18bf9cf3fdf52d02ea96ffe1aa0df05f4d
Justin Littman <justinlittman@stanford.edu>
2025-03-03 13:36:46 -0500

    Do not link shareable links when they are not ready.
    closes #781

--- app/components/collections/show/deposits_component.rb
+++ app/components/collections/show/deposits_component.rb
@@ -22,31 +22,22 @@ module Collections
       end
 
       def values_for(work)
+        presenter = work_presenter_for(work)
         [
           helpers.link_to(work.title, work_or_wait_path(work)),
           work.user.name,
-          status_for(work),
+          presenter.status_message,
           work.object_updated_at ? helpers.l(work.object_updated_at, format: '%b %d, %Y') : nil,
-          persistent_link_for(work)
+          presenter.sharing_link
         ]
       end
 
-      def status_for(work)
-        presenter = WorkPresenter.new(work:,
-                                      version_status: work_statuses.fetch(work.druid, VersionStatus::NilStatus.new),
-                                      work_form: WorkForm.new)
-
-        presenter.status_message
-      end
-
-      def persistent_link_for(work)
-        if work.druid.nil?
-          ''
-        elsif work.doi_assigned?
-          helpers.link_to(nil, Doi.url(druid: work.druid))
-        else
-          helpers.link_to(nil, Sdr::Purl.from_druid(druid: work.druid))
-        end
+      def work_presenter_for(work)
+        WorkPresenter.new(work:,
+                          version_status: work_statuses.fetch(
+                            work.druid, VersionStatus::NilStatus.new
+                          ),
+                          work_form: WorkForm.new(druid: work.druid))
       end
     end
   end
--- app/components/elements/tables/row_component.rb
+++ app/components/elements/tables/row_component.rb
@@ -7,16 +7,17 @@ module Elements
       renders_many :cells
       renders_many :items
 
-      def initialize(label: nil, values: [], id: nil)
+      def initialize(label: nil, values: [], id: nil, tooltip: nil)
         @label = label
         # Provide either values, cells (e.g. for content files),
         # or items (list of values for a field such as related links )
         @values = values
         @id = id
+        @tooltip = tooltip
         super()
       end
 
-      attr_reader :label, :values, :id
+      attr_reader :label, :values, :id, :tooltip
 
       def empty_cell?
         label.present? && values.empty? && !items? && !cells?
