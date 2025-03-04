b7bae6ec270d10c19601442be9d7b4137b782b34
Justin Littman <justinlittman@stanford.edu>
2025-03-04 07:43:39 -0500

    Refactors toggle component and fixes accessibility.
    closes #901

--- app/components/elements/forms/toggle_component.rb
+++ app/components/elements/forms/toggle_component.rb
@@ -4,26 +4,12 @@ module Elements
   module Forms
     # Component for a toggle-like radio button group field
     class ToggleComponent < FieldComponent
-      renders_one :left_toggle_option, lambda { |form:, field_name:, label:, value:, label_data: {}, data: {}|
-        label_data[:action] = merge_actions(label_data[:action], 'click->toggle#leftSelected')
-        label_data[:toggle_target] = 'leftLabel'
-        data[:toggle_target] = 'leftRadio'
-        Elements::Forms::ToggleOptionComponent.new(form:, field_name:, label:, value:, data:, label_data:)
-      }
-      renders_one :right_toggle_option, lambda { |form:, field_name:, label:, value:, label_data: {}, data: {}|
-        label_data[:action] = merge_actions(label_data[:action], 'click->toggle#rightSelected')
-        label_data[:toggle_target] = 'rightLabel'
-        data[:toggle_target] = 'rightRadio'
-        Elements::Forms::ToggleOptionComponent.new(form:, field_name:, label:, value:, data:, label_data:)
-      }
+      renders_one :left_toggle_option, Elements::Forms::ToggleOptionComponent
+      renders_one :right_toggle_option, Elements::Forms::ToggleOptionComponent
 
       def initialize(**args)
         args[:label_classes] = merge_classes('d-block', args[:label_classes])
         super
-      end
-
-      def data
-        { controller: 'toggle' }
       end
     end
   end
--- spec/components/elements/forms/toggle_component_spec.rb
+++ spec/components/elements/forms/toggle_component_spec.rb
@@ -10,11 +10,9 @@ RSpec.describe Elements::Forms::ToggleComponent, type: :component do
   let(:component) { described_class.new(form:, field_name:, label:) }
 
   before do
-    component.with_left_toggle_option(form:, field_name:, label: 'Type 1', value: 'type1', data: { test: 'test_data' },
-                                      label_data: { action: 'label_test_data1' })
+    component.with_left_toggle_option(form:, field_name:, label: 'Type 1', value: 'type1', data: { test: 'test_data' })
     component.with_right_toggle_option(form:, field_name:, label: 'Type 2', value: 'type2',
-                                       data: { test: 'more_test_data' },
-                                       label_data: { action: 'label_test_data2' })
+                                       data: { test: 'more_test_data' })
   end
 
   it 'creates toggle field with label' do
@@ -22,7 +20,6 @@ RSpec.describe Elements::Forms::ToggleComponent, type: :component do
     expect(page).to have_css('label.form-label:not(.visually-hidden)', text: 'Role Type')
     expect(page).to have_css('input[type="radio"]:not(.is-invalid)')
     expect(page).to have_css('input[data-test="test_data"]')
-    expect(page).to have_css('label[data-action="label_test_data1 click->toggle#leftSelected"]')
     expect(page).to have_no_css('p.form-text')
     expect(page).to have_no_css('div.invalid-feedback.is-invalid')
   end
