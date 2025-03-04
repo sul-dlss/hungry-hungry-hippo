ec74569d65e24abcdb115c6098694ef81969d60e
Justin Littman <justinlittman@stanford.edu>
2025-03-04 14:03:41 -0500

    Change previous button to primary outline.
    closes #926

--- app/components/edit/tab_form/previous_button_component.rb
+++ app/components/edit/tab_form/previous_button_component.rb
@@ -11,7 +11,7 @@ module Edit
       end
 
       def call
-        render Elements::ButtonComponent.new(label: 'Previous', variant: :primary, data:, classes: @classes)
+        render Elements::ButtonComponent.new(label: 'Previous', variant: :'outline-primary', data:, classes: @classes)
       end
 
       def data
