40cb6dcddf5c747472a244f7d74413839364e460
Justin Littman <justinlittman@stanford.edu>
2025-03-14 07:34:37 -0400

    Fix cancel button for works and collections.
    closes #1073

--- app/components/collections/edit/pane_component.rb
+++ app/components/collections/edit/pane_component.rb
@@ -6,16 +6,25 @@ module Collections
     class PaneComponent < ::Edit::TabForm::PaneComponent
       renders_one :deposit_button # If not provided will render Next button
 
+      def initialize(collection_presenter:, **pane_args)
+        @collection_presenter = collection_presenter
+        super(**pane_args)
+      end
+
       # Preset footer slot consisting of cancel button plus next or deposit button.
       def footer
         tag.div(class: 'd-flex') do
-          concat render Elements::CancelButtonComponent.new(link: dashboard_path, classes: 'me-2')
+          concat render Elements::CancelButtonComponent.new(link: cancel_path, classes: 'me-2')
           if deposit_button?
             concat deposit_button
           else
             concat render ::Edit::TabForm::NextButtonComponent.new(tab_id:)
           end
         end
+      end
+
+      def cancel_path
+        @collection_presenter.nil? ? dashboard_path : collection_path(@collection_presenter)
       end
     end
   end
