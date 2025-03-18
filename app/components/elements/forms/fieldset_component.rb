9621d7b3f55ce92556bb32d3d1d792a63a9ced55
Nick Budak <budak@stanford.edu>
2025-03-18 16:05:14 -0700

    Mark work type option as required

--- app/components/elements/forms/fieldset_component.rb
+++ app/components/elements/forms/fieldset_component.rb
@@ -8,7 +8,7 @@ module Elements
       renders_one :help_link # Optional
 
       def initialize(label: nil, hidden_label: false, classes: [], label_classes: [], legend_classes: [], # rubocop:disable Metrics/ParameterLists
-                     data: {}, id: nil, tooltip: nil, aria: {})
+                     data: {}, id: nil, tooltip: nil, aria: {}, required: false)
         @label = label
         @hidden_label = hidden_label
         @classes = classes
@@ -18,10 +18,11 @@ module Elements
         @legend_classes = legend_classes
         @tooltip = tooltip
         @aria = aria
+        @required = required
         super()
       end
 
-      attr_reader :label, :data, :id, :tooltip, :aria
+      attr_reader :label, :data, :id, :tooltip, :aria, :required
 
       def label_classes
         merge_classes(@label_classes, @hidden_label ? 'visually-hidden' : 'form-label fw-bold')
