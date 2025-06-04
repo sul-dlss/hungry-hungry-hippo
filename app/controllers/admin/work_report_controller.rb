ef51056011a12809cf4bd0f2f6656d58dbd6bbbc
Laura Wrubel <lwrubel@stanford.edu>
2025-06-04 13:56:07 -0400

    Start filtering works by params

--- app/controllers/admin/work_report_controller.rb
+++ app/controllers/admin/work_report_controller.rb
@@ -8,21 +8,18 @@ module Admin
 
       @collections = Collection.pluck(:title, :id)
       @work_report_form = Admin::WorkReportForm.new
-      @states = states
     end
 
     def create
       authorize!
 
       @work_report_form = Admin::WorkReportForm.new(**work_report_params)
-      # If the form is submitted, provide a confirmation
       if @work_report_form.valid?
         Admin::WorkReport.call(work_report_form: @work_report_form)
         flash[:success] = I18n.t('messages.work_report_generated')
         redirect_to new_admin_work_report_path
       else
         @collections = Collection.pluck(:title, :id)
-        @states = states
         render :new, status: :unprocessable_entity
       end
     end
@@ -30,13 +27,7 @@ module Admin
     private
 
     def work_report_params
-      params.permit(:date_created_start)
-    end
-
-    def states
-      [['Draft - not deposited', 'draft'], ['Pending approval', 'pending_approval'],
-       ['Returned', 'returned'], ['Deposit in progress', 'deposit-in_progress'], ['Deposited', 'deposited'],
-       ['New version in draft', 'new_version_in_draft']]
+      params.expect(:admin_work_report)
     end
   end
 end
