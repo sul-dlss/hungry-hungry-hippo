9545b004e9e08bbe08870ff6e30a46a97d7d6492
Laura Wrubel <lwrubel@stanford.edu>
2025-06-04 12:53:04 -0400

    Update last_deposited_at

--- app/jobs/deposit_work_job.rb
+++ app/jobs/deposit_work_job.rb
@@ -130,12 +130,10 @@ class DepositWorkJob < ApplicationJob
       Sdr::Repository.accession(druid:, new_user_version: new_user_version?)
       # If a collection manager or reviewer has rejected a previous review, we need to approve the work again
       work.approve! if work.rejected_review?
+      update_last_deposited_at!
     else
       work.deposit_persist_complete!
-      update_last_deposited_at!
     end
-
-    update_last_deposited_at!
   end
 
   def update_last_deposited_at!
