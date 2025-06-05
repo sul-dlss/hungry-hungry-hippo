fc3d8bcd8f43ec3064aafccabfb86eb3f151e94c
Peter Mangiafico <peter@mangiafico.org>
2025-06-05 10:02:40 -0700

    undo spinner changes

--- spec/requests/wait_globus_spec.rb
+++ spec/requests/wait_globus_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe 'Wait globus' do
 
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('<turbo-frame id="globus"')
-      expect(response.body).to include('Transferring your files from Globus')
+      expect(response.body).to include('Getting list of files from Globus')
     end
   end
 end
