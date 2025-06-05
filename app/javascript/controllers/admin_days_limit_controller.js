ab7b943edd5dee83a3acfa654956a96f6af9f5ca
Aaron Collier <aaron.collier@stanford.edu>
2025-06-05 14:40:06 -0700

    Submit the form when the days are selected

--- app/javascript/controllers/admin_days_limit_controller.js
+++ app/javascript/controllers/admin_days_limit_controller.js
@@ -3,9 +3,7 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['daysLimit']
 
-  connect () {
-    if (this.daysLimitTarget.changed) {
-      console.log("Days limit has changed, resetting to prompt option.")
-    }
+  change (event) {
+    event.target.form.requestSubmit()
   }
 }
