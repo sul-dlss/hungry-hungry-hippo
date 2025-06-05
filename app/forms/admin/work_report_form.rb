45d90d739a6889c3a5ffda711ecc610b7b23e2b1
Laura Wrubel <lwrubel@stanford.edu>
2025-06-05 12:06:12 -0400

    Form submission through to getting work attributes

--- app/forms/admin/work_report_form.rb
+++ app/forms/admin/work_report_form.rb
@@ -4,11 +4,17 @@ module Admin
   # Admin form object for generating work reports
   class WorkReportForm < ApplicationForm
     include ActiveModel::Attributes
+
     attribute :date_created_start, :date
     attribute :date_created_end, :date
     attribute :date_modified_start, :date
     attribute :date_modified_end, :date
-    attribute :collection_ids, default: []
-    attribute :state, :string
+    attribute :collection_ids
+    attribute :draft_not_deposited_state, :string, default: '0'
+    attribute :pending_review_state, :string, default: '0'
+    attribute :returned_state, :string, default: '0'
+    attribute :deposit_in_progress_state, :string, default: '0'
+    attribute :deposited_state, :string, default: '0'
+    attribute :version_draft_state, :string, default: '0'
   end
 end
--- app/services/admin/work_report.rb
+++ app/services/admin/work_report.rb
@@ -12,39 +12,67 @@ module Admin
     end
 
     def call
-      # query H3 by the params and get works
-      items = filter_by_state(works)
+      # query works by data/collection params and get version statuses
+      statuses = Sdr::Repository.statuses(druids: works_subset_druids)
+      druids = filter_by_state(statuses)
+
+      cocina_objects = druids.map { |druid| Dor::Services::Client.object(druid).find }
+      cocina_objects.map do |cocina_object|
+        Form::WorkMapper.call(cocina_object:, doi_assigned: true,
+                              agree_to_terms: true,
+                              version_description: nil, collection: nil)
+      end
 
       # TODO:
-      # query DSA by druids to get cocina
-      # parse cocina into CSV
-      # mail the result
+      # any additional parsing of cocina into CSV
+      # mail the result (put this in the job)
     end
 
     attr_reader :work_report_form
 
     private
 
-    def works
+    # finds works that match the form's date and collection criteria
+    # @return [Array<String>] druids
+    def works_subset_druids
       Work.where(
-        date_created: work_report_form.date_created_start..work_report_form.date_created_end,
-        date_modified: work_report_form.date_modified_start..work_report_form.date_modified_end,
+        created_at: work_report_form.date_created_start..work_report_form.date_created_end,
+        object_updated_at: work_report_form.date_modified_start..work_report_form.date_modified_end,
         collection_id: work_report_form.collection_ids
-      ).pluck(:druid, :title, :created_at, :date_modified, :state)
+      ).pluck(:druid)
     end
 
-    def filter_by_state
-      # filter to checked checkboxes
-      selected_states = work_report_form.attributes.select { |_, value| value == 1 }.keys
+    def selected_states
+      # filter to checked checkboxes (the only checkboxes are for item states)
+      work_report_form.attributes.select { |key, value| key.end_with?('state') && value == '1' }.keys
+    end
 
-      return works if selected_states.empty?
+    # @param [Array<Sdr::Repository::ObjectVersion::VersionStatus>] statuses
+    # @return [Array<String>] druids
+    def filter_by_state(statuses)
+      return statuses.map(&:first) if selected_states.empty?
 
-      works.select do |work|
-        if selected_states.include?('draft_not_deposited')
-          work.first_draft?
-          # need to add other states
+      # for each version status, check if the work matches any selected state
+      statuses.select do |status|
+        selected_states.each do |selected_state|
+          case selected_state
+          when 'draft_not_deposited'
+            status.first_draft?
+          when 'pending_review'
+            status.work.pending_review?
+          when 'returned'
+            status.work.rejected_review?
+          when 'deposit_in_progress'
+            status.accessioning?
+          when 'version_draft'
+            status.open?
+          when 'deposited'
+            !status.first_draft? && !status.open? && !status.accessioning?
+          else
+            false
+          end
         end
-      end
+      end.map(&:first)
     end
   end
 end
