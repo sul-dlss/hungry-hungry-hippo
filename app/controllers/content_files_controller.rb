29cf4e0826d88935713fd48271f25edc40285dda
Peter Mangiafico <peter@mangiafico.org>
2025-06-16 16:37:12 -0700

    download file from staging, stacks or preservation

--- app/controllers/content_files_controller.rb
+++ app/controllers/content_files_controller.rb
@@ -32,14 +32,74 @@ class ContentFilesController < ApplicationController
   def download
     authorize! @content_file
 
-    staging_filepath = StagingSupport.staging_filepath(druid: @content_file.content.work.druid,
-                                                       filepath: @content_file.filepath)
-    return head :bad_request unless File.exist?(staging_filepath)
+    status = Sdr::Repository.status(druid:)
 
-    send_file staging_filepath, filename: @content_file.filename, type: @content_file.mime_type
+    # if the object is accessioned and the file is not hidden, get it from the stacks
+    if status.openable? && !@content_file.hide
+      download_from_stacks
+    # if the object is accessioned, get it from preservation
+    elsif status.openable?
+      download_from_preservation(status.version)
+    # otherwise, get it from the staging area
+    else
+      download_from_staging
+    end
   end
 
   private
+
+  # rubocop:disable Metrics/AbcSize
+  # this was borrowed from Argo without permission
+  def download_from_preservation(version)
+    # Set headers on the response before writing to the response stream
+    send_file_headers!(
+      type: 'application/octet-stream',
+      disposition: 'attachment',
+      filename: CGI.escape(filepath.split('/').last)
+    )
+    response.headers['Last-Modified'] = Time.now.utc.rfc2822 # HTTP requires GMT date/time
+
+    Preservation::Client.objects.content(
+      druid:,
+      filepath:,
+      version:,
+      on_data: proc { |data, _count| response.stream.write data }
+    )
+  rescue Preservation::Client::NotFoundError => e
+    # Undo the header setting above for the streaming response. Not relevant here.
+    response.headers.delete('Last-Modified')
+    response.headers.delete('Content-Disposition')
+
+    render status: :not_found, plain: "Preserved file not found: #{e}"
+  rescue Preservation::Client::Error => e
+    message = "Preservation client error getting content of #{filepath} for #{druid}: #{e}"
+    logger.error(message)
+    Honeybadger.notify(message)
+    render status: :internal_server_error, plain: message
+  ensure
+    response.stream.close
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def download_from_stacks
+    redirect_to "#{Settings.stacks.file_url}/#{druid}/#{filepath}", allow_other_host: true
+  end
+
+  def download_from_staging
+    staging_filepath = StagingSupport.staging_filepath(druid:, filepath:)
+
+    return head :bad_request unless File.exist?(staging_filepath)
+
+    send_file staging_filepath, filename: filepath, type: @content_file.mime_type
+  end
+
+  def druid
+    @content_file.work.druid
+  end
+
+  def filepath
+    @content_file.filepath
+  end
 
   def set_content_file
     @content_file = ContentFile.includes(:content).find(params[:id])
--- spec/requests/download_content_file_spec.rb
+++ spec/requests/download_content_file_spec.rb
@@ -3,10 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Download content file' do
-  let(:content_file) { create(:content_file, content:, mime_type: 'text/plain') }
+  let(:content_file) { create(:content_file, content:, mime_type: 'text/plain', hide:) }
   let(:user) { create(:user) }
   let(:work) { create(:work, :with_druid) }
   let(:content) { create(:content, work:, user:) }
+  let(:version_status) { build(:version_status) }
+  let(:hide) { false }
+  let(:druid) { work.druid }
+
+  before do
+    allow(Sdr::Repository).to receive(:status).with(druid:).and_return(version_status)
+    sign_in(:user)
+    sign_in(user)
+  end
 
   context 'when the user is not authorized' do
     before do
@@ -20,31 +29,58 @@ RSpec.describe 'Download content file' do
     end
   end
 
-  context 'when the file is not available for download' do
-    before do
-      sign_in(user)
+  context 'when the user is authorized' do
+    context 'when the file is not available for download' do
+      it 'returns a bad request' do
+        get "/content_files/#{content_file.id}/download"
+
+        expect(response).to have_http_status(:bad_request)
+      end
     end
 
-    it 'returns a bad request' do
-      get "/content_files/#{content_file.id}/download"
+    context 'when the file is available for download and is not yet accessioned' do
+      let(:version_status) { build(:first_draft_version_status) }
 
-      expect(response).to have_http_status(:bad_request)
+      before do
+        allow(StagingSupport).to receive(:staging_filepath).and_return('spec/fixtures/files/hippo.txt')
+      end
+
+      it 'returns the file from staging' do
+        get "/content_files/#{content_file.id}/download"
+
+        expect(response).to have_http_status(:ok)
+        expect(response.header['Content-Disposition']).to include('attachment')
+        expect(response.header['Content-Type']).to eq('text/plain')
+        expect(response.body).to eq(File.read('spec/fixtures/files/hippo.txt'))
+      end
     end
-  end
 
-  context 'when the file is available for download' do
-    before do
-      allow(StagingSupport).to receive(:staging_filepath).and_return('spec/fixtures/files/hippo.txt')
-      sign_in(user)
+    context 'when the file is available for download, is already accessioned and is not hidden' do
+      let(:version_status) { build(:openable_version_status) }
+
+      it 'returns the file from the stacks' do
+        get "/content_files/#{content_file.id}/download"
+        expect(response.location).to eq "#{Settings.stacks.file_url}/#{druid}/#{content_file.filepath}"
+      end
     end
 
-    it 'returns the file' do
-      get "/content_files/#{content_file.id}/download"
+    context 'when the file is available for download, is already accessioned and is hidden' do
+      let(:version_status) { build(:openable_version_status) }
+      let(:hide) { true }
 
-      expect(response).to have_http_status(:ok)
-      expect(response.header['Content-Disposition']).to include('attachment')
-      expect(response.header['Content-Type']).to eq('text/plain')
-      expect(response.body).to eq(File.read('spec/fixtures/files/hippo.txt'))
+      before do
+        allow(Preservation::Client.objects).to receive(:content)
+      end
+
+      it 'returns the file from preservation' do
+        get "/content_files/#{content_file.id}/download"
+        expect(Preservation::Client.objects).to have_received(:content).with(
+          druid:,
+          filepath: content_file.filepath,
+          version: version_status.version,
+          on_data: Proc
+        )
+      end
     end
   end
 end
