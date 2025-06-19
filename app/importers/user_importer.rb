55cbe6bd4c76d2e52542f1b95da05f02e2ea53ca
Peter Mangiafico <peter@mangiafico.org>
2025-06-19 14:14:44 -0700

    ensure agreed to columns is migrated

--- app/importers/user_importer.rb
+++ app/importers/user_importer.rb
@@ -14,6 +14,7 @@ class UserImporter
     ::User.find_or_create_by!(email_address: user_json['email']) do |user|
       user.name = user_json['name'] || user_json['email'].delete_suffix(::User::EMAIL_SUFFIX)
       user.first_name = user_json['first_name']
+      user.agreed_to_terms_at = user_json['last_work_terms_agreement']
     end
   end
 
