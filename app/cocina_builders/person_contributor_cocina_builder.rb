581f6d40c1bc3f37e07ebde9c5e4a39aee661ff2
Aaron Collier <aaron.collier@stanford.edu>
2025-06-20 10:07:11 -0700

    Contributor affiliations mapping WIP

--- app/cocina_builders/person_contributor_cocina_builder.rb
+++ app/cocina_builders/person_contributor_cocina_builder.rb
@@ -11,12 +11,13 @@ class PersonContributorCocinaBuilder
   # @param role [String] the role of the person from ROLES
   # @param primary [Boolean] whether this is the first contributor
   # @param orcid [String] the ORCID of the person
-  def initialize(surname:, forename:, role:, primary: false, orcid: nil)
+  def initialize(surname:, forename:, role:, primary: false, orcid: nil, affiliations: []) # rubocop:disable Metrics/ParameterLists
     @surname = surname
     @forename = forename
     @role = role
     @primary = primary
     @orcid = orcid
+    @affiliations = affiliations
   end
 
   def call
@@ -32,15 +33,14 @@ class PersonContributorCocinaBuilder
       type: 'person',
       role: role_params,
       identifier: identifier_params,
-      status: ('primary' if primary)
-
-      # NOTE: affiliations.map { |affiliation_attrs| affiliation(**affiliation_attrs) }.presence
+      status: ('primary' if primary),
+      note: (ContributorAffiliationsCocinaBuilder.call(affiliations:) unless affiliations.empty?)
     }.compact
   end
 
   private
 
-  attr_reader :forename, :surname, :role, :primary, :orcid
+  attr_reader :forename, :surname, :role, :primary, :orcid, :affiliations
 
   def role_params
     cocina_role = ContributorRoleCocinaBuilder.call(role:)
