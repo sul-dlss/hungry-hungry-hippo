2ffbfe45e01bce69e6afa0717a04f4e0b6f77702
Peter Mangiafico <peter@mangiafico.org>
2025-06-27 15:16:09 -0700

    allow admin sorting of recent works

---
+++ app/components/elements/sort_dropdown_component.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Elements
+  # Component for rendering a dropdown of sorting options.
+  class SortDropdownComponent < ApplicationComponent
+    renders_many :sort_options, lambda { |label:, link:|
+      tag.li do
+        link_to label, link, class: 'dropdown-item'
+      end
+    }
+
+    def initialize(classes: [])
+      @classes = classes
+      super()
+    end
+
+    def classes
+      merge_classes('dropdown', @classes)
+    end
+  end
+end
