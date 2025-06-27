b515977fd4192ff3aff9f6f794f1d1f8f88b06e1
Peter Mangiafico <peter@mangiafico.org>
2025-06-27 13:00:49 -0700

    add comments about component use

--- app/components/elements/tables/row_component.rb
+++ app/components/elements/tables/row_component.rb
@@ -7,12 +7,17 @@ module Elements
       renders_many :cells
 
       def initialize(label: nil, first_value: nil, values: [], id: nil, tooltip: nil)
+        # Provide either label or first_value but not both; these are rendered in the first column
+        # label renders with <th> (bold), first_value is a normal <td>
         @label = label
+        @first_value = first_value
         # Provide either values or cells (e.g. for content files).
         @values = values
-        @first_value = first_value
         @id = id
         @tooltip = tooltip
+
+        raise ArgumentError if label.present? && first_value.present?
+
         super()
       end
 
