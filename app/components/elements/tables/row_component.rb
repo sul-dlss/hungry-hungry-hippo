a7dd251a0fedebf5f81f15d2620979f58092715c
Peter Mangiafico <pmangiafico@stanford.edu>
2025-06-27 13:41:10 -0700

    allow first value to be different weight from label (#1559)
    * allow first value to be different weight from label
    
    * add comments about component use

--- app/components/elements/tables/row_component.rb
+++ app/components/elements/tables/row_component.rb
@@ -6,16 +6,22 @@ module Elements
     class RowComponent < ApplicationComponent
       renders_many :cells
 
-      def initialize(label: nil, values: [], id: nil, tooltip: nil)
+      def initialize(label: nil, first_value: nil, values: [], id: nil, tooltip: nil)
+        # Provide either label or first_value but not both; these are rendered in the first column
+        # label renders with <th> (bold), first_value is a normal <td>
         @label = label
+        @first_value = first_value
         # Provide either values or cells (e.g. for content files).
         @values = values
         @id = id
         @tooltip = tooltip
+
+        raise ArgumentError if label.present? && first_value.present?
+
         super()
       end
 
-      attr_reader :label, :values, :id, :tooltip
+      attr_reader :label, :values, :id, :tooltip, :first_value
 
       def empty_cell?
         label.present? && values.empty? && !cells?
