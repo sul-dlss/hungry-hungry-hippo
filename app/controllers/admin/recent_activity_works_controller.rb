0e18609b8e57fc7014fe29482cba1e47b3eabde2
Peter Mangiafico <peter@mangiafico.org>
2025-06-27 15:16:09 -0700

    allow admin sorting of recent works

--- app/controllers/admin/recent_activity_works_controller.rb
+++ app/controllers/admin/recent_activity_works_controller.rb
@@ -8,20 +8,26 @@ module Admin
 
       @form = Admin::RecentActivityForm.new(**recent_activity_params)
       @rows = rows
+      @sort_by = sort_by
+      @days_limit = days_limit
     end
 
     private
 
     def days_limit
-      params.fetch(:admin_recent_activity, {}).fetch(:days_limit, 7).to_i
+      recent_activity_params.fetch(:days_limit, Admin::RecentActivityForm::DEFAULT_DAYS_LIMIT).to_i
     end
 
     def recent_activity_params
-      params.permit(:days_limit)
+      params.fetch(:admin_recent_activity, {}).permit(:days_limit)
+    end
+
+    def sort_by
+      params[:sort_by] || 'updated_at DESC'
     end
 
     def rows
-      Work.where('updated_at > ?', days_limit.days.ago).order('updated_at DESC').map do |work|
+      Work.where('updated_at > ?', days_limit.days.ago).order(sort_by).map do |work|
         Admin::RecentActivityWorkPresenter.values_for(work:)
       end
     end
