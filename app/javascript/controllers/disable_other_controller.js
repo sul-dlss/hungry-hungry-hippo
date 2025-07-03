3957aae346e7e7098eb81e5526cacb5d0cca4f76
Justin Littman <justinlittman@stanford.edu>
2025-07-03 07:32:48 -0400

    Remove cruft from disable other controller.

--- app/javascript/controllers/disable_other_controller.js
+++ app/javascript/controllers/disable_other_controller.js
@@ -4,10 +4,6 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['other']
 
-  connect () {
-    console.debug('DisableOtherController connected', this.otherTargets)
-  }
-
   disable (event) {
     this.otherTargets.forEach((target) => {
       if (target !== event.target) {
