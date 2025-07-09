8701a13b40b2a5e577590ab38a108966670ab181
Michael J. Giarlo <mjgiarlo@stanford.edu>
2025-07-09 12:52:29 -0700

    Remove left/right padding from icon buttons
    Fixes #335
    
    This commit effectively reduces the amount of horizontal padding between icon buttons and adjacent fields, having the desired effect of bringing the field delete button and the text field for repeatable components (e.g., contact email) closer. It also removes the horizontal padding of the move up and move down buttons, which I checked out in the work edit screen for authors, and it does not meaningfully impact that part of the UI as the delete and move buttons are arranged vertically there.

--- app/components/elements/delete_button_component.rb
+++ app/components/elements/delete_button_component.rb
@@ -4,7 +4,7 @@ module Elements
   # Component for a delete button
   class DeleteButtonComponent < IconButtonComponent
     def initialize(label: 'Clear', **args)
-      super(icon: :delete, label:, **args)
+      super(icon: :delete, label:, classes: Array(classes) + ['px-0'], **args)
     end
   end
 end
