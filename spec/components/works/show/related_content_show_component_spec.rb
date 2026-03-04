af817e3e564c5167c9a0a3cfd25f4e8456b3e46e
Justin Littman <justinlittman@stanford.edu>
2026-03-04 12:12:12 -0700

    Show blank related published work on work show page.
    closes #2125

--- spec/components/works/show/related_content_show_component_spec.rb
+++ spec/components/works/show/related_content_show_component_spec.rb
@@ -4,11 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Works::Show::RelatedContentShowComponent, type: :component do
   let(:related_work) { RelatedWorkForm.new(citation:, identifier:, relationship:) }
+  let(:related_works) { [related_work] }
   let(:citation) { 'A related work' }
   let(:identifier) { 'https://example.org/related' }
   let(:relationship) { 'isCitedBy' }
   let(:work_presenter) do
-    WorkPresenter.new(work_form: WorkForm.new(related_works: [related_work]),
+    WorkPresenter.new(work_form: WorkForm.new(related_works:),
                       version_status: instance_double(VersionStatus, editable?: false),
                       work: instance_double(Work))
   end
@@ -33,6 +34,16 @@ RSpec.describe Works::Show::RelatedContentShowComponent, type: :component do
       expect(page).to have_no_link('https://example.org/related', href: 'https://example.org/related')
       expect(page).to have_content('A related work')
       expect(page).to have_content('Is Cited By')
+    end
+  end
+
+  context 'with no related works' do
+    let(:related_works) { [] }
+
+    it 'renders the related content section with a blank row' do
+      render_inline(described_class.new(work_presenter:))
+
+      expect(page).to have_content('Related published work')
     end
   end
 end
