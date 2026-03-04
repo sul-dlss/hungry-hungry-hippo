aecb8f6adfb7709cbe608ffb53e94603c2cb55fc
Aaron Collier <aaron.collier@stanford.edu>
2026-03-04 09:33:42 -0800

    Output related works as a link if it is a URL (#2107)

--- app/components/works/show/related_content_show_component.rb
+++ app/components/works/show/related_content_show_component.rb
@@ -4,6 +4,8 @@ module Works
   module Show
     # Component for rendering related contents on the work show page
     class RelatedContentShowComponent < ApplicationComponent
+      include LinkHelper
+
       def initialize(work_presenter:)
         @work_presenter = work_presenter
         super()
@@ -17,6 +19,19 @@ module Works
 
       def relationship_label_for(related_work)
         t("related_works.edit.fields.relationship.options.#{related_work.relationship}")
+      end
+
+      # Returns a link if the related work is a URL, otherwise returns the related work
+      # param related_work [RelatedWorkForm] the related work to generate a label for
+      # return [String, RelatedWorkForm] a link if the related work is a URL, otherwise the related work
+      def label_for(related_work)
+        label = related_work.to_s
+        uri = URI.parse(label)
+        return label unless %w[http https].include?(uri.scheme)
+
+        link_to_new_tab label, label
+      rescue URI::InvalidURIError, URI::BadURIError
+        related_work
       end
     end
   end
---
+++ spec/components/works/show/related_content_show_component_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Works::Show::RelatedContentShowComponent, type: :component do
+  let(:related_work) { RelatedWorkForm.new(citation:, identifier:, relationship:) }
+  let(:citation) { 'A related work' }
+  let(:identifier) { 'https://example.org/related' }
+  let(:relationship) { 'isCitedBy' }
+  let(:work_presenter) do
+    WorkPresenter.new(work_form: WorkForm.new(related_works: [related_work]),
+                      version_status: instance_double(VersionStatus, editable?: false),
+                      work: instance_double(Work))
+  end
+
+  context 'with a related work that has a URL' do
+    it 'renders the related work with a link' do
+      render_inline(described_class.new(work_presenter:))
+
+      expect(page).to have_content('Related published work')
+      expect(page).to have_link('https://example.org/related', href: 'https://example.org/related')
+      expect(page).to have_content('Is Cited By')
+    end
+  end
+
+  context 'with a related work that is not a URL' do
+    let(:identifier) { nil }
+
+    it 'renders the related work with a link' do
+      render_inline(described_class.new(work_presenter:))
+
+      expect(page).to have_content('Related published work')
+      expect(page).to have_no_link('https://example.org/related', href: 'https://example.org/related')
+      expect(page).to have_content('A related work')
+      expect(page).to have_content('Is Cited By')
+    end
+  end
+end
