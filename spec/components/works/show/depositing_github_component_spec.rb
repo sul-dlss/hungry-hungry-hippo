e0f7b6f7dccc9e52996bbd895669499dfce0caea
Justin Littman <justinlittman@stanford.edu>
2026-03-09 11:44:51 -0400

    Only show GH alert on show page when GH deposit is enabled. (#2164)
    closes #2163

--- spec/components/works/show/depositing_github_component_spec.rb
+++ spec/components/works/show/depositing_github_component_spec.rb
@@ -3,19 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe Works::Show::DepositingGithubComponent, type: :component do
+  let(:component) { described_class.new(work_presenter:) }
   let(:work) { create(:github_repository) }
   let(:work_presenter) do
-    WorkPresenter.new(work_form: WorkForm.new, version_status:, work:)
+    WorkPresenter.new(work_form: WorkForm.new(github_deposit_enabled:), version_status:, work:)
   end
+  let(:github_deposit_enabled) { true }
+  let(:version_status) { build(:first_draft_version_status) }
 
   context 'when first draft version' do
-    let(:version_status) { build(:first_draft_version_status) }
-
     it 'renders the alert' do
-      render_inline(described_class.new(work_presenter:))
+      render_inline(component)
 
       expect(page).to have_css('.alert', text: 'One more step to complete deposit - ' \
                                                'Go to GitHub to create a release for this GitHub repository.')
+      expect(page).to have_link('Go to GitHub', href: "https://github.com/#{work.github_repository_name}/releases")
     end
   end
 
@@ -23,10 +25,15 @@ RSpec.describe Works::Show::DepositingGithubComponent, type: :component do
     let(:version_status) { build(:accessioning_version_status) }
 
     it 'does not render the alert' do
-      render_inline(described_class.new(work_presenter:))
+      expect(component.render?).to be false
+    end
+  end
 
-      expect(page).to have_no_css('.alert', text: 'One more step to complete deposit - ' \
-                                                  'Go to GitHub to create a release for this GitHub repository.')
+  context 'when github deposit not enabled' do
+    let(:github_deposit_enabled) { false }
+
+    it 'does not render the alert' do
+      expect(component.render?).to be false
     end
   end
 end
