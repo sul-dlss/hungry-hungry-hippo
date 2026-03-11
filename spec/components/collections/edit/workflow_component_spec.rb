4a0117fceb19e5e4330c22158eb91ab6aae45065
Peter Mangiafico <pmangiafico@stanford.edu>
2026-03-11 14:08:56 -0700

    adjust github workflow messages (#2214)
    * adjust github workflow messages
    
    * adjust github message

--- spec/components/collections/edit/workflow_component_spec.rb
+++ spec/components/collections/edit/workflow_component_spec.rb
@@ -6,27 +6,14 @@ RSpec.describe Collections::Edit::WorkflowComponent, type: :component do
   subject(:component) { described_class.new(form:) }
 
   let(:form) { ActionView::Helpers::FormBuilder.new(nil, collection_form, vc_test_view_context, {}) }
-  let(:collection_form) { CollectionForm.new(github_deposit_enabled:) }
+  let(:collection_form) { CollectionForm.new }
   let(:informational_text) do
-    'If GitHub Integration Workflow is set to "No", new releases for existing GitHub ' \
-      'repositories will continue to be deposited.'
+    'Once a Github repository is set up for automatic depositing, new releases will continue to be deposited ' \
+      'unless turned off in the edit form for the individual item even if this workflow is set to "No".'
   end
 
-  context 'when github deposit is enabled' do
-    let(:github_deposit_enabled) { true }
-
-    it 'displays the informational text about new releases' do
-      render_inline(component)
-      expect(page).to have_text(informational_text)
-    end
-  end
-
-  context 'when github deposit is disabled' do
-    let(:github_deposit_enabled) { false }
-
-    it 'does not display the informational text about new releases' do
-      render_inline(component)
-      expect(page).to have_no_text(informational_text)
-    end
+  it 'displays the informational text about new releases' do
+    render_inline(component)
+    expect(page).to have_text(informational_text)
   end
 end
