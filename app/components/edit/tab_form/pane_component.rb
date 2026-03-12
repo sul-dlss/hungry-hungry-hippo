1dd904c8933f902be278ae2e56bb8e54a38de1d9
Peter Mangiafico <pmangiafico@stanford.edu>
2026-03-12 10:41:28 -0700

    allow certain tab panes to remove the class which adds the padding (#2210)

--- app/components/edit/tab_form/pane_component.rb
+++ app/components/edit/tab_form/pane_component.rb
@@ -8,16 +8,18 @@ module Edit
       renders_one :footer
       renders_one :help
 
-      def initialize(tab_name:, active_tab_name:, label: nil, help_text: nil, tooltip: nil)
+      def initialize(tab_name:, active_tab_name:, label: nil, help_text: nil, tooltip: nil, # rubocop:disable Metrics/ParameterLists
+                     pane_header_class: 'pane-header')
         @tab_name = tab_name
         @label = label
         @selected = tab_name == active_tab_name
         @help_text = help_text
         @tooltip = tooltip
+        @pane_header_class = pane_header_class
         super()
       end
 
-      attr_reader :tab_name, :selected, :label, :help_text, :tooltip
+      attr_reader :tab_name, :selected, :label, :help_text, :tooltip, :pane_header_class
 
       def classes
         merge_classes(%w[tab-pane fade h-100], selected ? 'show active' : nil)
