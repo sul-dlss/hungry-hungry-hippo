a348af1f7dfbcd713cf95bf00e9f8faaa80f3726
Peter Mangiafico <peter@mangiafico.org>
2026-03-13 13:01:43 -0700

    allow license help text to be hidden for articles

--- app/components/works/edit/license_component.rb
+++ app/components/works/edit/license_component.rb
@@ -4,17 +4,18 @@ module Works
   module Edit
     # Select field for license
     class LicenseComponent < ApplicationComponent
-      def initialize(form:, license_presenter:, license_help_url: Settings.license_url, show_terms_of_use: true,
-                     tooltip: nil)
+      def initialize(form:, license_presenter:, license_help_url: Settings.license_url, show_terms_of_use: true, # rubocop:disable Metrics/ParameterLists
+                     show_help_text: true, tooltip: nil)
         @form = form
         @license_presenter = license_presenter
         @license_help_url = license_help_url
         @show_terms_of_use = show_terms_of_use
+        @show_help_text = show_help_text
         @tooltip = tooltip
         super()
       end
 
-      attr_reader :form, :show_terms_of_use, :license_help_url, :tooltip
+      attr_reader :form, :show_terms_of_use, :show_help_text, :license_help_url, :tooltip
 
       delegate :required_license_option?, to: :@license_presenter
 
@@ -27,7 +28,7 @@ module Works
       end
 
       def help_text
-        helpers.t('work_form.fields.license.help_text')
+        show_help_text ? helpers.t('work_form.fields.license.help_text') : nil
       end
 
       def license_options
