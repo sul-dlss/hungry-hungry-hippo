59810988c30ff526760af713befd35fea0615c1f
Justin Littman <justinlittman@stanford.edu>
2026-08-06 15:34:02 -0400

    Change managers depositing email to only send one email per invocation.

--- spec/subscription_handlers/work_accessioning_started_subscription_mailer_spec.rb
+++ spec/subscription_handlers/work_accessioning_started_subscription_mailer_spec.rb
@@ -8,12 +8,15 @@ RSpec.describe WorkAccessioningStartedSubscriptionMailer, :active_job_test_adapt
   let(:current_user) { create(:user) }
 
   context 'when work' do
-    let(:work) { create(:work) }
+    let(:manager) { create(:user) }
+    let(:collection) { create(:collection, managers: [manager, current_user]) }
+    let(:work) { create(:work, collection:) }
 
-    it 'sends the deposited email' do
+    it 'sends the managers depositing email to each manager other than the current user' do
       described_class.call(object: work, current_user:)
 
-      assert_enqueued_email_with WorksMailer.with(work:, current_user:), :managers_depositing_email
+      assert_enqueued_email_with WorksMailer.with(work:, user: manager), :managers_depositing_email
+      assert_enqueued_emails 1
     end
   end
 
